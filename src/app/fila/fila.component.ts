6cd2e788548459117e10096f6f3bf4a67150092e
405153-Masera <405153@tecnicatura.frc.utn.edu.ar>
2024-09-14 13:25:51 -0300

    405153_Masera

---
+++ src/app/fila/fila.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fila',
+  standalone: true,
+  imports: [],
+  templateUrl: './fila.component.html',
+  styleUrl: './fila.component.css'
+})
+export class FilaComponent {
+
+}
