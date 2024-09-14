a190aadb2bd687a21eec41cdc81517a3a1581418
405153-Masera <405153@tecnicatura.frc.utn.edu.ar>
2024-09-14 17:39:49 -0300

    405153_Masera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { FileItem } from '../models/file.item.model';
 import { FILE_LIST } from '../data/file.storage';
 import { TablaComponent } from "./tabla/tabla.component";
+import { FormularioComponent } from './formulario/formulario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TablaComponent],
+  imports: [RouterOutlet, TablaComponent, FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   files: FileItem[] = FILE_LIST;
   title = 'file-management';
+  formularioActivo: boolean = false;
+
+  guardarArchivo(archivo: FileItem) {
+    this.files.push(archivo);
+    this.formularioActivo = false;
+  }
+
+  abrirFormulario() {
+    this.formularioActivo = true;
+  }
 }
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output} from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { FileItem, FileType } from '../../models/file.item.model';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  @Output() archivoCreado = new EventEmitter<FileItem>();
+  fileType = FileType;
+  fileItem: FileItem ={
+    id:"",
+    name: "",
+    creation: new Date(),
+    type: FileType.FILE,
+    owners: [],
+  };
 
+  crearArchivo(form: NgForm) {
+    this.archivoCreado.emit(this.fileItem);
+    form.reset();
+  }
+
+  volver($event: any){
+
+  }
 }
